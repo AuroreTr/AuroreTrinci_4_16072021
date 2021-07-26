8b4ed755f1402dcc91c12141b57a3dee7ca2906a
Aurore Trinci <aurore.trinci@gmail.com>
2021-07-26 19:59:00 +0200

    beginning of JS code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,37 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+document.getElementById('close').addEventListener('click', function() {
+  modalbg.style.display = 'none';
+});
+
+
+
+// const minLength = 2;
+
+// function stringLength(inputText, errorMessage) {
+  // if(inputText < minLength) {
+  //   errorMessage.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
+  //   e.preventDefault();
+//   }
+// }
+
+let reserveForm = document.getElementById('reserveForm');
+
+reserveForm.addEventListener('submit', function(e) {
+  console.log('ok')
+  let firstNameInput = document.getElementById('first-name');
+  // let lastNameInput = document.getElementById('last-name');
+  let nameMessageError = document.getElementsByClassName('name');
+  // stringLength(firstNameInput, nameMessageError);
+  // stringLength(lastNameInput, nameMessageError);
+  if(firstNameInput < '2') {
+    console.log('Veuillez entrer 2 caractères ou plus pour le champ du nom.');
+    // e.preventDefault();
+  }
+})
+
+
+
 
